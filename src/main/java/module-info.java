a7395f96adcc8755da318d6c31983d69be0fd822
sungvzer <sungvzer@proton.me>
2026-03-03 23:14:32 +0100

    chore: update to Java25

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,9 @@
-module WeendTray {
+open module WeendTray {
     requires bcrypt;
     requires java.sql;
     requires javafx.controls;
     requires javafx.fxml;
+    requires info.picocli;
 
-    exports it.salvatoregargano.weendtray to javafx.graphics;
-    opens it.salvatoregargano.weendtray to javafx.fxml;
-
+    exports it.salvatoregargano.weendtray to javafx.graphics, info.picocli;
 }
