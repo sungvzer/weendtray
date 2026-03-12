36e29fe4223f64945283524b185edec8fbb018e1
Antonio Volpe <antoniovolpe785@gmail.com>
2026-03-13 00:42:20 +0100

    Added controller to call tab

---
+++ src/main/java/it/salvatoregargano/weendtray/ui/callTabController.java
@@ -0,0 +1,26 @@
+package it.salvatoregargano.weendtray.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class callTabController {
+    @FXML
+    private TextField numberTextField;
+
+    @FXML
+    private Label callStateLabel;
+
+    @FXML
+    private Button callButton;
+
+    @FXML
+    private Button hangUpButton;
+
+    @FXML
+    private Label callTimeLabel;
+
+    @FXML
+    private Label costLabel;
+}
