1cfcfd3fcbf9452bd4f2e0fb89bc4abb2d195ad1
Antonio Volpe <antoniovolpe785@gmail.com>
2026-03-12 18:40:00 +0100

    User dashboard for subscribed user

---
+++ src/main/java/it/salvatoregargano/weendtray/ui/UserDashboardController.java
@@ -0,0 +1,8 @@
+package it.salvatoregargano.weendtray.ui;
+
+import it.salvatoregargano.weendtray.acl.User;
+import it.salvatoregargano.weendtray.telephone.billing.WalletService;
+
+
+public class UserDashboardController {
+}
