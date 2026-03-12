e7513c421125233c91dfae82dd98b228392d8ac0
Salvio Gargano <sungvzer@proton.me>
2026-03-12 23:40:04 +0100

    implement data reading in user dashboard

--- src/main/java/it/salvatoregargano/weendtray/ui/UserDashboardController.java
+++ src/main/java/it/salvatoregargano/weendtray/ui/UserDashboardController.java
@@ -1,18 +1,17 @@
 package it.salvatoregargano.weendtray.ui;
 
-import it.salvatoregargano.weendtray.acl.*;
+import java.sql.SQLException;
+
+import it.salvatoregargano.weendtray.acl.CredentialsService;
+import it.salvatoregargano.weendtray.acl.RegularUser;
 import it.salvatoregargano.weendtray.telephone.billing.UserAccountKind;
 import it.salvatoregargano.weendtray.telephone.billing.Wallet;
 import it.salvatoregargano.weendtray.telephone.billing.WalletService;
-
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
-
-import java.sql.SQLException;
-
 
 public class UserDashboardController {
 
@@ -30,6 +29,12 @@ public class UserDashboardController {
 
     @FXML
     private Label renewalLabel;
+
+    @FXML
+    private Label creditLabel;
+
+    @FXML
+    private Label userPhonePlan;
 
     @FXML
     private VBox usageBox;
@@ -62,16 +67,31 @@ public class UserDashboardController {
         userWallet = WalletService.getInstance().getWallet(user.getId());
         var userPlanKind = user.getKind();
 
-        if (userPlanKind.equals(UserAccountKind.PAY_AS_YOU_GO)){
+        userPhonePlan.setText(userPhonePlan.getText() + " " + user.getPhonePlan().toString());
+        userPlanType.setText(userPlanType.getText() + userPlanKind.getName());
+        creditLabel.setText(creditLabel.getText() + String.format(" %.2f €", userWallet.getBalance()));
+
+        if (userPlanKind.equals(UserAccountKind.PAY_AS_YOU_GO)) {
             usageBox.setManaged(false);
             usageBox.setVisible(false);
             renewalLabel.setManaged(false);
             renewalLabel.setVisible(false);
+        } else {
+            userDataTraffic
+                    .setText(userWallet.getDataCount() + " MB" + " / " + user.getPhonePlan().getDataLimitMB() + " MB");
+            userCallMinutes.setText(
+                    userWallet.getMinutesCount() + " min" + " / " + user.getPhonePlan().getMinutesLimit() + " min");
+            userMessagesAmount.setText(
+                    userWallet.getMessagesCount() + " sms" + " / " + user.getPhonePlan().getMessagesLimit() + " sms");
+            dataProgress.setProgress(userWallet.getDataCount() / user.getPhonePlan().getDataLimitMB());
+            callProgress.setProgress(userWallet.getMinutesCount() / (double) user.getPhonePlan().getMinutesLimit());
+            smsProgress.setProgress(userWallet.getMessagesCount() / (double) user.getPhonePlan().getMessagesLimit());
+
+            renewalLabel
+                    .setText(renewalLabel.getText()
+                            + String.format(" %.2f €/mese", user.getPhonePlan().getRenewalCost()));
 
         }
-
-        userPlanType.setText(userPlanType.getText() + userPlanKind.getName());
-        userDataTraffic.setText();
 
     }
 }
